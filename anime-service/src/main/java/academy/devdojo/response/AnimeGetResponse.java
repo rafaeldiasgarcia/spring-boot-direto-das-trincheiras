91ac6b7ef5d571c1dd57a43ce6b4c1da1ab08880
William Suane <william.suane@devdojo.academy>
2024-07-23 10:56:52 +0200

    video20: exercise 04 MapStruct

---
+++ anime-service/src/main/java/academy/devdojo/response/AnimeGetResponse.java
@@ -0,0 +1,15 @@
+package academy.devdojo.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Builder
+public class AnimeGetResponse {
+    private Long id;
+    private String name;
+}
