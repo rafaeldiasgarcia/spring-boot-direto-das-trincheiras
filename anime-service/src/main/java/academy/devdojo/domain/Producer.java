ddf4040ca079926c06bb1c91fcdb98dd74b67cd4
William Suane <william.suane@devdojo.academy>
2024-07-26 09:50:47 +0200

    video26: MVC pattern 01 repository

--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -20,17 +18,5 @@ public class Producer {
     @JsonProperty("name")
     private String name;
     private LocalDateTime createdAt;
-    private static List<Producer> producers = new ArrayList<>();
 
-    static {
-        var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
-        var kyotoAnimation = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
-        var madhouse = Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
-        producers.addAll(List.of(mappa, kyotoAnimation, madhouse));
-    }
-
-
-    public static List<Producer> getProducers() {
-        return producers;
-    }
 }
