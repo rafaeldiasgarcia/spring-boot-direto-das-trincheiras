f7cf473e3f38deb8b5fc42f7eeaa8db65dddb79a
William Suane <william.suane@devdojo.academy>
2024-08-09 11:45:08 +0200

    video46: unit test controller with code pt 07

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -23,7 +23,7 @@ public class AnimeController {
     private final AnimeService service;
 
     @GetMapping
-    public ResponseEntity<List<AnimeGetResponse>> listAll(@RequestParam(required = false) String name) {
+    public ResponseEntity<List<AnimeGetResponse>> findAll(@RequestParam(required = false) String name) {
         log.debug("Request received to list all animes, param name '{}'", name);
 
         var animes = service.findAll(name);
