c9783fba738ee7e4e966a08579bad5ced5da5fc3
William Suane <william.suane@devdojo.academy>
2024-08-13 10:28:56 +0200

    video49: spring profiles 01

--- anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
+++ anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
@@ -1,15 +1,22 @@
 package academy.devdojo.config;
 
 import external.dependency.Connection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectionConfiguration {
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.username}")
+    private String username;
+    @Value("${database.password}")
+    private String password;
 
     @Bean
     public Connection connectionMySql() {
-        return new Connection("localhost", "devdojoMySQL", "goku");
+        return new Connection(url, username, password);
     }
 
     @Bean(name = "connectionMongoDB")
---
+++ anime-service/src/main/java/academy/devdojo/controller/ConnectionController.java
@@ -0,0 +1,22 @@
+package academy.devdojo.controller;
+
+import external.dependency.Connection;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/connections")
+@Slf4j
+@RequiredArgsConstructor
+public class ConnectionController {
+    private final Connection connectionMySql;
+
+    @GetMapping
+    public ResponseEntity<Connection> getConnections() {
+        return ResponseEntity.ok(connectionMySql);
+    }
+}
