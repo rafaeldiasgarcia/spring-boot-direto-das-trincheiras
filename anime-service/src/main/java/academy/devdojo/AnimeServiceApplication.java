3b5b36f35894be9175be12fbe54086478dc9ef99
William Suane <william.suane@devdojo.academy>
2024-09-11 10:12:30 +0200

    video81: exercise 17 pt 01

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -1,12 +1,9 @@
 package academy.devdojo;
 
-import academy.devdojo.config.ConnectionConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ConnectionConfigurationProperties.class)
 public class AnimeServiceApplication {
 
     public static void main(String[] args) {
--- anime-service/src/main/java/academy/devdojo/config/ConnectionConfigurationProperties.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "database")
-public record ConnectionConfigurationProperties(String url, String username, String password) {
-}
