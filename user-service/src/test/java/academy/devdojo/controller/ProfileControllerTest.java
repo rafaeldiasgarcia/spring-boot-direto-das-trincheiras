b45d713029a776b555db098eae3da319670ae12e
William Suane <william.suane@devdojo.academy>
2024-09-23 10:46:31 +0200

    video92: exercise profile pt 02

---
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerTest.java
@@ -0,0 +1,137 @@
+package academy.devdojo.controller;
+
+import academy.devdojo.commons.FileUtils;
+import academy.devdojo.commons.ProfileUtils;
+import academy.devdojo.domain.Profile;
+import academy.devdojo.respository.ProfileRepository;
+import academy.devdojo.respository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@WebMvcTest(controllers = ProfileController.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ComponentScan(basePackages = {"academy.devdojo"})
+class ProfileControllerTest {
+    private static final String URL = "/v1/profiles";
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ProfileRepository repository;
+    @MockBean
+    private UserRepository userRepository;
+    private List<Profile> profileList;
+    @Autowired
+    private FileUtils fileUtils;
+    @Autowired
+    private ProfileUtils profileUtils;
+
+    @BeforeEach
+    void init() {
+        profileList = profileUtils.newProfileList();
+    }
+
+    @Test
+    @DisplayName("GET v1/profiles returns a list with all profiles")
+    @Order(1)
+    void findAll_ReturnsAllProfiles_WhenSuccessful() throws Exception {
+        BDDMockito.when(repository.findAll()).thenReturn(profileList);
+        var response = fileUtils.readResourceFile("profile/get-profiles-200.json");
+
+        mockMvc.perform(MockMvcRequestBuilders.get(URL))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("GET v1/profiles returns empty list when nothing is not found")
+    @Order(2)
+    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() throws Exception {
+        var response = fileUtils.readResourceFile("profile/get-profiles-empty-list-200.json");
+
+        mockMvc.perform(MockMvcRequestBuilders.get(URL))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("POST v1/profiles creates an profile")
+    @Order(3)
+    void save_CreatesProfile_WhenSuccessful() throws Exception {
+        var request = fileUtils.readResourceFile("profile/post-request-profile-200.json");
+        var response = fileUtils.readResourceFile("profile/post-response-profile-201.json");
+        var profileSaved = profileUtils.newProfileSaved();
+
+        BDDMockito.when(repository.save(ArgumentMatchers.any())).thenReturn(profileSaved);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post(URL)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @ParameterizedTest
+    @MethodSource("postProfileBadRequestSource")
+    @DisplayName("POST v1/profiles returns bad request when fields are invalid")
+    @Order(11)
+    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String fileName, List<String> errors) throws Exception {
+        var request = fileUtils.readResourceFile("profile/%s".formatted(fileName));
+
+        var mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .post(URL)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
+
+        var resolvedException = mvcResult.getResolvedException();
+
+        Assertions.assertThat(resolvedException).isNotNull();
+
+        Assertions.assertThat(resolvedException.getMessage()).contains(errors);
+    }
+
+    private static Stream<Arguments> postProfileBadRequestSource() {
+        var allRequiredErrors = allRequiredErrors();
+
+        return Stream.of(
+                Arguments.of("post-request-profile-empty-fields-400.json", allRequiredErrors),
+                Arguments.of("post-request-profile-blank-fields-400.json", allRequiredErrors)
+        );
+    }
+
+    private static List<String> allRequiredErrors() {
+        var nameRequiredError = "The field 'name' is required";
+        var descriptionRequiredError = "The field 'description' is required";
+
+        return new ArrayList<>(List.of(nameRequiredError, descriptionRequiredError));
+    }
+
+}
