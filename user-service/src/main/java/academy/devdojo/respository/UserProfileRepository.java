a4904af9dfd12db5dea0d0b3007eab358de35503
William Suane <william.suane@devdojo.academy>
2024-09-26 11:05:30 +0200

    video97: queries com JQPL

--- user-service/src/main/java/academy/devdojo/respository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/respository/UserProfileRepository.java
@@ -2,6 +2,12 @@ package academy.devdojo.respository;
 
 import academy.devdojo.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    @Query("SELECT up FROM UserProfile up join fetch up.user u join fetch up.profile p")
+    List<UserProfile> retrieveAll();
 }
