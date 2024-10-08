e1b34e25de15786930d33398d4d1a1cf3e5119fa
William Suane <william.suane@devdojo.academy>
2024-10-08 11:28:27 +0200

    video105: integration tests with TestRestTemplate pt 02

--- user-service/src/main/java/academy/devdojo/response/ProfilePostResponse.java
+++ user-service/src/main/java/academy/devdojo/response/ProfilePostResponse.java
@@ -1,12 +1,12 @@
 package academy.devdojo.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfilePostResponse {
     private Long id;
 }
--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
@@ -1,18 +1,24 @@
 package academy.devdojo.controller;
 
+import academy.devdojo.commons.FileUtils;
+import academy.devdojo.commons.ProfileUtils;
+import academy.devdojo.domain.Profile;
 import academy.devdojo.response.ProfileGetResponse;
+import academy.devdojo.response.ProfilePostResponse;
+import academy.devdojo.respository.ProfileRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,15 +27,19 @@ class ProfileControllerIT {
     private static final String URL = "/v1/profiles";
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private ProfileUtils profileUtils;
+    @Autowired
+    private FileUtils fileUtils;
 
     @Test
     @DisplayName("GET v1/profiles returns a list with all profiles")
     @Sql(value = "/sql/init_two_profiles.sql")
     @Order(1)
-    void findAll_ReturnsAllProfiles_WhenSuccessful() throws Exception {
+    void findAll_ReturnsAllProfiles_WhenSuccessful() {
         var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>() {};
 
-        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
+        var responseEntity = testRestTemplate.exchange(URL, GET, null, typeReference);
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isNotNull().doesNotContainNull();
@@ -37,5 +47,39 @@ class ProfileControllerIT {
         responseEntity
                 .getBody()
                 .forEach(profileGetResponse -> assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
+    }
+
+    @Test
+    @DisplayName("GET v1/profiles returns empty list when nothing is not found")
+    @Order(2)
+    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() {
+        var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>() {};
+
+        var responseEntity = testRestTemplate.exchange(URL, GET, null, typeReference);
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isNotNull().isEmpty();
+    }
+
+    @Test
+    @DisplayName("POST v1/profiles creates an profile")
+    @Order(3)
+    void save_CreatesProfile_WhenSuccessful() throws Exception {
+        var request = fileUtils.readResourceFile("profile/post-request-profile-200.json");
+        var profileEntity = buildHttpEntity(request);
+
+        var responseEntity = testRestTemplate.exchange(URL, POST, profileEntity, ProfilePostResponse.class);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(responseEntity.getBody()).isNotNull().hasNoNullFieldsOrProperties();
+    }
+
+
+    private static HttpEntity<String> buildHttpEntity(String request) {
+        var httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        return new HttpEntity<>(request, httpHeaders);
     }
 }
