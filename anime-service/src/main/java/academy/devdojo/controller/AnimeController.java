c8e40dedf7bf2a26709349cc80373dcab53d1577
rafaeldiasgarcia <rafdgar@gmail.com>
2026-03-09 01:21:08 -0300

    video 10

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,5 +1,6 @@
 package academy.devdojo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/animes")
+@Slf4j
 public class AnimeController {
 
     @GetMapping
     public List<String> listAll() {
 
+        log.info(Thread.currentThread().getName());
         return List.of("Ninja Kamui", "Kaijuu-8gou");
     }
 }
