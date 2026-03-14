6c1af9c4f1ea1a17554abc55dfb7100545815248
rafaeldiasgarcia <rafdgar@gmail.com>
2026-03-14 15:20:16 -0300

    video 12

--- anime-service/src/main/java/academy/devdojo/controller/HeroController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HeroController.java
@@ -1,9 +1,6 @@
 package academy.devdojo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,12 +19,27 @@ public class HeroController {
     @GetMapping("/filter")
     public List<String> listAllHeroesParam(@RequestParam(defaultValue = "") String name) {
 
-        return HEROES.stream().filter(hero -> hero.equalsIgnoreCase(name)).toList();
+        return HEROES
+                .stream()
+                .filter(hero -> hero.equalsIgnoreCase(name))
+                .toList();
     }
 
     @GetMapping("/filterList")
     public List<String> listAllHeroesParamList(@RequestParam List<String> names) {
 
-        return HEROES.stream().filter(names::contains).toList();
+        return HEROES
+                .stream()
+                .filter(names::contains)
+                .toList();
+    }
+
+    @GetMapping("/{name}")
+    public String findByName(@PathVariable String name) {
+
+        return HEROES
+                .stream()
+                .filter(hero -> hero.equalsIgnoreCase(name))
+                .findFirst().orElse("");
     }
 }
