d6370745c4ce348665315aa3bfded5221ca76324
rafaeldiasgarcia <rafdgar@gmail.com>
2026-03-14 16:09:15 -0300

    video 15

--- anime-service/src/main/java/academy/devdojo/domain/Anime.java
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -2,22 +2,31 @@ package academy.devdojo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class Anime {
 
-    Long id;
-    String name;
+    private Long id;
+    private String name;
+    private static List<Anime> animes = new ArrayList<>();
 
-    public static List<Anime> getAnimes() {
+    static {
 
         Anime ninjaKamui = new Anime(1L, "Ninja Kamui");
         Anime kaijuu = new Anime(2L, "Kaijuu-8gou");
         Anime kimetsuNoYaiba = new Anime(3L, "Kimetsu No Yaiba");
 
-        return List.of(ninjaKamui, kaijuu, kimetsuNoYaiba);
+        animes.addAll(List.of(ninjaKamui, kaijuu, kimetsuNoYaiba));
+    }
+
+    public static List<Anime> getAnimes() {
+
+        return animes;
     }
 }
