5b1102f8eb29cf9fa3a214ae8d20112d47354b05
rafaeldiasgarcia <rafdgar@gmail.com>
2026-03-14 16:31:00 -0300

    video 17

---
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -0,0 +1,32 @@
+package academy.devdojo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Producer {
+
+    private Long id;
+    private String name;
+    private static List<Producer> producers = new ArrayList<>();
+
+    static {
+
+        Producer mappa = new Producer(1L, "Mappa");
+        Producer kyotoAnimation = new Producer(2L, "Kyoto Animation");
+        Producer madhouse = new Producer(3L, "Madhouse");
+
+        producers.addAll(List.of(mappa, kyotoAnimation, madhouse));
+    }
+
+    public static List<Producer> getProducers() {
+
+        return producers;
+    }
+}
