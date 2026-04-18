172462c354d510b3a465d47fea64fbf486ccece9
rafaeldiasgarcia <rafdgar@gmail.com>
2026-04-18 01:21:03 -0300

    video 24

---
+++ anime-service/src/main/java/academy/devdojo/request/AnimePutRequest.java
@@ -0,0 +1,15 @@
+package academy.devdojo.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class AnimePutRequest {
+
+    private Long id;
+    private String name;
+}
+
