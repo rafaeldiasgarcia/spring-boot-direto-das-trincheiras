f270027f255a2c8f5f109cf50ef97f26f92abd83
rafaeldiasgarcia <rafdgar@gmail.com>
2026-04-18 01:42:36 -0300

    video 26

--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -22,33 +22,5 @@ public class Producer {
     @JsonProperty("nmame")
     private String name;
     private LocalDateTime createdAt;
-    private static List<Producer> producers = new ArrayList<>();
-
-    static {
-
-        Producer mappa = Producer.builder()
-                .id(1L)
-                .name("Mappa")
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        Producer kyotoAnimation = Producer.builder()
-                .id(2L)
-                .name("Kyoto Animation")
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        Producer madhouse = Producer.builder()
-                .id(3L)
-                .name("Madhouse")
-                .createdAt(LocalDateTime.now())
-                .build();
-
-        producers.addAll(List.of(mappa, kyotoAnimation, madhouse));
-    }
-
-    public static List<Producer> getProducers() {
-
-        return producers;
-    }
+    
 }
